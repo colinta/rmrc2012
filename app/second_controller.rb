ef4b5f04d1f1f8d56e45963ee59249a5eb5b8af0
Colin Thomas-Arnold <colinta@gmail.com>
2012-09-21 12:20:30 -0600

    basic UITableViewDelegate and UITableViewDataSource (note: you do not declare protocol adherence.  just write your code)

--- app/second_controller.rb
+++ app/second_controller.rb
@@ -6,4 +6,51 @@ class SecondViewController < UIViewController
     end
   end
 
+  layout :root do
+    subview(UITableView.grouped, delegate: self, dataSource: self)
+  end
+
+  def viewDidLoad
+    super
+    @data = %w{Lorem ipsum dolor sit amet consectetur adipiscing elit}
+  end
+
+  ##|
+  ##|  UITableView DELEGATE/DATASOURCE
+  ##|
+  def numberOfSectionsInTableView(table_view)
+    1
+  end
+
+  def tableView(table_view, titleForHeaderInSection:section)
+    "Lorem Ipsum"
+  end
+
+  def tableView(table_view, numberOfRowsInSection:section)
+    case section
+    when 0
+      @data.size
+    end
+  end
+
+  def tableView(table_view, cellForRowAtIndexPath:index_path)
+    if index_path.row == 0
+      cell_identifier = 'SecondViewController - lorem cell'
+      cell = table_view.dequeueReusableCellWithIdentifier(cell_identifier)
+
+      if not cell
+        cell = UITableViewCell.alloc.initWithStyle(:default.uitablecellstyle,
+                            reuseIdentifier: cell_identifier)
+      end
+
+      row = @data[index_path.row]
+      cell.textLabel.text = row
+    end
+
+    return cell
+  end
+
+  def tableView(table_view, didSelectRowAtIndexPath:index_path)
+    table_view.deselectRowAtIndexPath(index_path, animated:true)
+  end
 end
