eaaa434af5346fb73294e4d8c3ea61672f6388f1
Colin Thomas-Arnold <colinta@gmail.com>
2012-09-21 10:28:36 -0600

    a glimpse of teacup emerges!  we use the Teacup::UIViewController class method `layout` to create our view hierarchy.  This method is called during `viewDidLoad`

--- app/first_controller.rb
+++ app/first_controller.rb
@@ -6,4 +6,19 @@ class FirstViewController < UIViewController
     end
   end
 
+  # use teacup to create a button and style it.
+  layout do
+    button = subview(UIButton.rounded_rect, :button,
+
+      # `frame` is a property on UIView,
+      # and so this assignment is the same as `button.frame = [...]`
+      frame: [[10, 10], [100, 20]],
+
+      # the `title:` method doesn't exist on UIButton, it is assigned
+      # using a "teacup_handler".  easier to read, at the cost of a
+      # little bit of obfuscation
+      title: "Hi!",
+      )
+  end
+
 end
